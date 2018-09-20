88452158672e46b14b7b8a81eeeb3ff3bd598277
alldoami <adoami@ucla.edu>
2018-09-20 13:36:45 -0700

    formatting with pylint

--- tesla_challenge_allison.py
+++ tesla_challenge_allison.py
@@ -1,19 +1,19 @@
-from flask import Flask
 import collections
-import unittest
 from string import ascii_lowercase
 from string import ascii_uppercase
+import unittest
+from flask import Flask
 
 app = Flask(__name__)
 
-allNodes = {}
+all_nodes = {}
 
 class TestDijkstra(unittest.TestCase):
     def testAddNode(self):
         print("testaddnode")
         #Testing all letters
         for c in ascii_lowercase:
-            self.assertEqual(AddNode(c),c)
+            self.assertEqual(AddNode(c), c)
 
         #Testing attempt to add an already existant node
         self.assertEqual(AddNode('a'), "Node a already in graph")
@@ -21,7 +21,7 @@ class TestDijkstra(unittest.TestCase):
     def testAddRoute(self):
         print("testAddRoute")
         i = 0
-        #Testing to add routes to all 
+        #Testing to add routes to all
         while i < len(ascii_lowercase)-1:
             self.assertEqual(AddRoute(ascii_lowercase[i], ascii_lowercase[i+1], 1), ascii_lowercase[i] + '->' + ascii_lowercase[i+1]+ ': ' + str(1))
             i += 1
@@ -42,18 +42,18 @@ class TestDijkstra(unittest.TestCase):
 
         #Example Given in Specs:
         AddNode('A')
-        AddNode('B') 
+        AddNode('B')
         AddNode('C')
-        AddNode('D') 
+        AddNode('D')
         AddNode('E')
-        AddRoute('B','D',10) 
-        AddRoute('B','C',5) 
-        AddRoute('A','B',4) 
-        AddRoute('A','C',2) 
-        AddRoute('C','E',3) 
-        AddRoute('E','D',4) 
-        AddNode('F') 
-        AddRoute('D','F',11) 
+        AddRoute('B', 'D', 10)
+        AddRoute('B', 'C', 5)
+        AddRoute('A', 'B', 4)
+        AddRoute('A', 'C', 2)
+        AddRoute('C', 'E', 3)
+        AddRoute('E', 'D', 4)
+        AddNode('F')
+        AddRoute('D', 'F', 11)
         self.assertEqual(GetFastestRoute('A', 'F'), '"route":"A->C->E->D->F", \n"distance":20')
 
         #Test Invalid Nodes (one that doesn't exist)
@@ -64,38 +64,38 @@ class TestDijkstra(unittest.TestCase):
 
 @app.route("/AddNode/<string:name>/")
 def AddNode(name):
-    if(name not in allNodes):
-        allNodes[name] = {}
+    if name not in all_nodes:
+        all_nodes[name] = {}
         return name
-    else:
-        return "Node " + name + " already in graph"
+    return "Node " + name + " already in graph"
 
 @app.route("/AddRoute/<string:firstNode>/<string:secondNode>/<int:length>/")
 def AddRoute(firstNode, secondNode, length):
-    if(firstNode in allNodes and secondNode in allNodes):
-        allNodes[firstNode][secondNode] = length
+    if firstNode in all_nodes and secondNode in all_nodes:
+        all_nodes[firstNode][secondNode] = length
         return firstNode + "->" + secondNode + ": " + str(length)
-    else:
-        return "Invalid route"
+    return "Invalid route"
 
-@app.route("/GetFastestRoute/<string:start>/<string:end>/", methods = ['GET'])
+@app.route("/GetFastestRoute/<string:start>/<string:end>/")
 def GetFastestRoute(start, end):
-    if(start not in allNodes.keys() or end not in allNodes.keys()):
+    if start not in all_nodes.keys() or end not in all_nodes.keys():
         return "Not a possible route"
     distance = {}
     distance[start] = 0
     route = {}
     q = collections.deque()
 
-    #Giving all nodes (besides the start) a temporary distance of infinity (meaning they haven't been visited yet)
-    for key, value in allNodes.items():
+    #Giving all nodes (besides the start) a temporary distance of infinity
+    #(meaning they haven't been visited yet)
+    for key, value in all_nodes.items():
         if key != start:
             distance[key] = float("inf")
         q.append(key)
-    
+
     #Go through queue of nodes and assign shortest path to each node
     while q:
-        #Need to sort distance dict because we want to find the node with the shortest tentative distance and set it as the current node
+        #Need to sort distance dict because we want to find the node with the shortest tentative distance and
+        #set it as the current node
         list_distance = sorted(distance.items(), key=lambda kv: kv[1])
         for i in range(len(list_distance)):
             if list_distance[i][0] in q:
@@ -104,12 +104,12 @@ def GetFastestRoute(start, end):
                 break
 
         #Goes through the nodes connected to the current node and finds the shortest path
-        for key, value in allNodes[v].items():
+        for key, value in all_nodes[v].items():
             d = distance[v] + value
             if d < distance[key]:
-                if (v != start):
+                if v != start:
                     route[key] = route[v] + '->' + key
-                else: 
+                else:
                     route[key] = v + '->' + key
                 distance[key] = d
 
@@ -121,21 +121,21 @@ def GetFastestRoute(start, end):
 
 def main():
     AddNode('A')
-    AddNode('B') 
+    AddNode('B')
     AddNode('C')
     AddNode('D')
     AddNode('E')
-    AddRoute('B','D',10) 
-    AddRoute('B','C',5) 
-    AddRoute('A','B',4) 
-    AddRoute('A','C',2) 
-    AddRoute('C','E',3) 
-    AddRoute('E','D',4) 
-    AddNode('F') 
-    AddRoute('D','F',11)
-    d = GetFastestRoute('A','F')
+    AddRoute('B', 'D', 10)
+    AddRoute('B', 'C', 5)
+    AddRoute('A', 'B', 4)
+    AddRoute('A', 'C', 2)
+    AddRoute('C', 'E', 3)
+    AddRoute('E', 'D', 4)
+    AddNode('F')
+    AddRoute('D', 'F', 11)
+    d = GetFastestRoute('A', 'F')
     print(d)
-        
+
 if __name__ == "__main__":
     #Runs main()
     #main()
@@ -144,6 +144,4 @@ if __name__ == "__main__":
     #unittest.main()
 
     #Runs Http server at http://127.0.0.1:5000
-    app.run() 
-
-                
+    app.run()
